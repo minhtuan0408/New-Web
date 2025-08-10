920c80bfeee317df1835fb4f261a6ca13cdba710
TuanBow <chugautoinghiep@gmail.com>
2025-08-10 19:51:31 +0700

    Components

---
+++ src/components/MyComponent.js
@@ -0,0 +1,21 @@
+// class component
+// functional component
+import React from 'react';
+
+class MyComponent extends React.Component {
+    
+    //JSX
+    render() {
+        return (    
+            <div>My first component
+                {Math.random()}
+                    
+                
+            </div>
+            
+        );
+    }
+}
+
+
+export default MyComponent;
